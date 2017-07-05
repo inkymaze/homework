c36b866be5773bf3bf546efe0c8e19c3cb956092
Nicholas Cannon <nickwcannon@gmail.com>
2017-07-04 20:31:02 -0700

    First AJAX
    w6d2

---
+++ first_AJAX/js/index.js
@@ -0,0 +1,20 @@
+console.log("Hello from the JavaScript console!");
+
+// Your AJAX request here
+
+// Add another console log here, outside your AJAX request
+
+$.ajax({
+  type: 'GET',
+  url:
+  "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
+  success(data) {
+    console.log("It's about to rain")
+    console.log(data);
+  },
+  error() {
+    console.log("Error");
+  },
+});
+
+console.log("Completed call");
