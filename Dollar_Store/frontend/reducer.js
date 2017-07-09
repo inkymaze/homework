1b63be909cfb0c84eba23ead54aedc31324976a6
Nicholas Cannon <nickwcannon@gmail.com>
2017-07-09 15:47:05 -0700

    Redux
    w7d1

---
+++ Dollar_Store/frontend/reducer.js
@@ -0,0 +1,26 @@
+import merge from 'lodash/merge';
+
+const initialState = {
+  baseCurrency: "Please select",
+  rates: {}
+};
+
+
+
+const reducer = (state = initialState, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case "SWITCH_CURRENCY":
+      let nextState = merge({}, state);
+      nextState.baseCurrency = action.baseCurrency;
+      nextState.rates = action.rates;
+      return nextState;
+
+
+    default:
+    return state;
+
+  }
+};
+
+export default reducer;
