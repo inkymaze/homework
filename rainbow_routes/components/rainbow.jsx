0dbe6286ae09c47ddea98f3dc824e895230fdb47
Nicholas Cannon <nickwcannon@gmail.com>
2017-07-13 07:17:46 -0700

    Redux Router - "Rainbow Routes"

---
+++ rainbow_routes/components/rainbow.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  Route,
+  Link,
+  NavLink
+} from 'react-router-dom';
+
+import Red from './red';
+import Green from './green';
+import Blue from './blue';
+import Violet from './violet';
+
+class Rainbow extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Rainbow Router!</h1>
+
+        <NavLink to="/red">Red only</NavLink>
+        <NavLink to="/green">Green</NavLink>
+        <NavLink to="/blue">Blue only</NavLink>
+        <NavLink to="/violet">Violet</NavLink>
+
+
+        <div id="rainbow">
+
+          <Route path="/red" component={Red} />
+          <Route path="/green" component={Green} />
+          <Route path="/blue" component={Blue} />
+          <Route path="/violet" component={Violet} />
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Rainbow;
